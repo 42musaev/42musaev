2752e18f6609620364a7de9720686577fbb5e8c0
Askhab Musaev <42musaev@gmail.com>
2024-07-20 12:14:47 +0300

    add broker

--- auth/api/auth.py
+++ auth/api/auth.py
@@ -3,10 +3,8 @@ from fastapi import APIRouter
 from fastapi import Cookie
 from fastapi import Depends
 from schemas.jwt_token import TokenSchema
-from schemas.user import UserEmailSchema
 from schemas.user import UserSchema
 from services.token import create_pair_tokens
-from services.token import get_my_email
 from services.token import update_pair_tokens
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
@@ -33,8 +31,3 @@ async def refresh_token(
     session: AsyncSession = Depends(get_db_session),
 ):
     return await update_pair_tokens(request, response, refresh_token, session)
-
-
-@router.post('/protected', response_model=UserEmailSchema)
-async def get_info_about_met(user_email: UserSchema = Depends(get_my_email)):
-    return UserEmailSchema(email=user_email)
